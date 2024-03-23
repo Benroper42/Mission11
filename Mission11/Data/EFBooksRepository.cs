c127dca781fee7773436b50b3ee3b6502063e040
Ben Roper <benroper42@gmail.com>
2024-03-22 22:52:24 -0600

    Add project files.

---
+++ Mission11/Data/EFBooksRepository.cs
@@ -0,0 +1,16 @@
+﻿using Mission11.Models;
+
+namespace Mission11.Data
+{
+    public class EfBooksRepository : IBooksRepository
+    {
+        private BookstoreContext _context;
+
+        public EfBooksRepository(BookstoreContext temp)
+        {
+            _context = temp;
+        }
+
+        public IQueryable<Book> Books => _context.Books;
+    }
+}
---
+++ Mission11/Data/IBooksRepository.cs
@@ -0,0 +1,9 @@
+﻿using Mission11.Models;
+
+namespace Mission11.Data
+{
+    public interface IBooksRepository
+    {
+        public IQueryable<Book> Books { get; }
+    }
+}
